4f9afd86c3f7284bd2ac41f19460a204738c42d7
Toni Schmidbauer <toni@stderr.at>
2020-10-11 17:16:46 +0200

    import bean lifecycle demo

--- src/at/stderr/springdemo/BeanLifeCycleDemo.java
+++ src/at/stderr/springdemo/BeanLifeCycleDemo.java
@@ -7,20 +7,11 @@ public class BeanLifeCycleDemo {
 	public static void main(String[] args) {
 		// load spring configuration
 		ClassPathXmlApplicationContext context =
-				new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+				new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
 			
 		// retrieve bean from spring container
 		Coach theCoach = context.getBean("myCoach", Coach.class);
-		Coach alphaCoach = context.getBean("myCoach", Coach.class);
-		
-		// check if this are the same  beans
-		boolean result = (theCoach == alphaCoach);
-		
-		//print out result
-		System.out.println("\nPointing to the same object: " + result);
-		
-		System.out.println("\nMemory location for theCoach: " + theCoach);
-		System.out.println("\nMemory location for alphaCoach: " + alphaCoach + "\n");
+		System.out.println(theCoach.getDailyWorkout() );
 		
 		context.close();
 	}
